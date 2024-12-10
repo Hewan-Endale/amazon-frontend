fb5515d4b7df5d67488a3c28d773deea14a9ecc6
Hewan-Endale <Hewanendale2@gmail.com>
2024-12-11 01:05:27 +0300

    baseurlchange

--- src/API/axios.js
+++ src/API/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  // baseURL: "http://localhost:5000",
+
+  baseURL: "https://amazon-backend-deploy-bk17.onrender.com",
 });
 
 export { axiosInstance };
